9ac59938ce83c368e45497c543465847faf60a53
Victoria-2 <114245919+Victoria-2@users.noreply.github.com>
2024-11-24 22:23:59 -0300

    Creacion card como widget

--- lib/screens/pokemon_list.dart
+++ lib/screens/pokemon_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pokemon/widgets/create_pokemon_card.dart';
 import 'package:flutter_pokemon/widgets/menu.dart';
+import 'package:flutter_pokemon/mocks/pokemon_mock.dart';
 
 class PokemonList extends StatelessWidget {
   const PokemonList({super.key});
@@ -21,17 +23,17 @@ class PokemonList extends StatelessWidget {
   }
 
   GridView listarPokemones() {
-    return GridView.count(
-      crossAxisCount: 2,
-      
-      children: List.generate(50, (index) { // aca poner el listview builder
-        return Center(
-          child: Text(
-            'Item $index',
-          ),
-          );
-      },
-      ),
-    );
-  }
+  return GridView.count(
+    crossAxisCount: 2, // num columnas
+    children: List.generate(elements.length, (index) {
+      var pokemon = elements[index];
+      int id = pokemon[0];
+      String name = pokemon[1];
+      int xp = pokemon[2];
+      String sprite = pokemon[3];
+
+      return PokemonCard(id: id, name: name, xp: xp, sprite: sprite);
+    }),
+  );
 }
+  }
